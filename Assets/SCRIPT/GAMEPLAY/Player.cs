310680c65d73a1da903168923884e14c57bacf33
Nala Mthembu <Nala00.12@gmail.com>
2022-11-09 15:08:49 +0200

    Iteration Base Movement
    yes, it kinda works

--- Assets/SCRIPT/GAMEPLAY/Player.cs
+++ Assets/SCRIPT/GAMEPLAY/Player.cs
@@ -1,101 +1,55 @@
-using static Managers.InputManager;
 using UnityEngine;
 
 
 namespace Gameplay
 {
-    public enum PLAYER_NO
-    {
-        ONE,
-        TWO,
-        THREE,
-        FOUR
-    }
-
-    [RequireComponent(typeof(CharacterController))]
     public class Player : MonoBehaviour
     {
-        [SerializeField] PLAYER_NO m_PlayerNumber;
-        [SerializeField][Range(0, 20)] float m_MovementSpeed = 7f;
-        [SerializeField][Range(-50, 0)] float m_Gravity = -12f;
-        [SerializeField][Range(0, 20)] float m_JumpHeight = 2;
-        [SerializeField] LayerMask m_GroundLayers = -1;
+        CharacterController m_CharacterController;
 
-        CharacterController m_Controller;
-        Vector3 m_Velocity;
-        Vector3 m_Origin;
-        float m_VelocityY;
-        readonly float rayLength = 0.08f;
+        [SerializeField][Range(1, 10)] float m_MovementSpeed = 2;
+        [SerializeField][Range(1, 20)] float m_HitForce = 2;
 
-        int JumpCount = 0; //FOR DOUBLE JUMP.
-
-        RaycastHit m_Hit;
-        bool m_IsGrounded = true;
-
-        private void Awake()
+        void Start()
         {
-            m_Controller = GetComponent<CharacterController>();
+            m_CharacterController = GetComponent<CharacterController>();
         }
 
         private void Update()
         {
-            if (m_Controller is null)
-                return;
+            Vector2 input = new
+                (
+                    Input.GetAxisRaw("Horizontal"),
+                    Input.GetAxisRaw("Vertical")
+                );
 
-            Move();
+            Vector2 inputNormalised = input.normalized;
+
+            float inputMagnitude = inputNormalised.magnitude;
+
+            Vector3 velocity = Vector3.right * inputNormalised.x + Vector3.forward * inputNormalised.y;
+
+            velocity *= (m_MovementSpeed * 2) * inputMagnitude;
+
+            velocity *= Time.deltaTime;
+
+            velocity += 10 * Time.deltaTime * Vector3.down;
+
+            m_CharacterController.Move(velocity);
         }
 
-        private void Move()
+        private void OnTriggerEnter(Collider other)
         {
-            m_Velocity = (INPUT_MANAGER.InputXY.x * Vector3.right) * m_MovementSpeed;
+            Rigidbody rb = other.GetComponent<Rigidbody>();
 
-            m_VelocityY += m_Gravity * Time.deltaTime;
-
-            if (INPUT_MANAGER.JumpKey)
-                Jump();
-
-            m_Velocity += Vector3.up * m_VelocityY;
-
-            m_Controller.Move(m_Velocity * Time.deltaTime);
-
-            m_Origin = transform.position;
-
-            m_IsGrounded = Physics.Raycast(m_Origin, -transform.up, out m_Hit, rayLength, m_GroundLayers);
-
-            if (m_IsGrounded)
+            if (other.CompareTag("Ball") && rb is not null)
             {
-                if (JumpCount >= 2)
-                    JumpCount = 0;
+                rb.transform.position = transform.position + Vector3.up * m_CharacterController.height;
+                rb.velocity = default;
+                rb.AddForce((transform.forward + transform.up) * (m_HitForce * 2), ForceMode.Impulse);
 
-                m_VelocityY = 0;
+                Debug.Log("Hit!");
             }
-
-            Debug.DrawLine(m_Origin, m_Origin + Vector3.down * rayLength, Color.magenta);
-        }
-
-        private void Jump()
-        {
-            if (JumpCount >= 2)
-            {
-                if (m_IsGrounded)
-                {
-                    JumpCount = 0;
-                    return;
-                }
-
-                return;
-            }
-
-            m_VelocityY = Mathf.Sqrt(-2 * m_Gravity * m_JumpHeight);
-            JumpCount++;
-        }
-
-        private void OnControllerColliderHit(ControllerColliderHit hit)
-        {
-            if (!hit.rigidbody)
-                return;
-
-            hit.rigidbody.AddForce(-hit.normal * transform.position.magnitude, ForceMode.Impulse);
         }
     }
 }
